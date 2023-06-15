6c3eccaa1c8e9b9d873279606fe554b1c7b28933
masteroi <master2005@walla.com>
2023-06-15 15:31:13 +0300

    Appending JSDocs. Continue dev

--- myPromise.js
+++ myPromise.js
@@ -1,9 +1,19 @@
+/**
+ * Global param for state. to retrieve the status.
+ */
+
 const STATE = {
   PENDING: 'pending',
   FULFILLED: 'fulfilled',
   REJECTED: 'rejected',
 };
 
+/**
+ * Create a new custom of myPromise implementation.
+ * @param executor callback used to initialize the promise. This callback is passed two arguments:
+ * a resolve callback used to resolve the promise with a value or the result of another promise,
+ * and a reject callback used to reject the promise with a provided reason or error.
+ */
 class myPromise {
   #thenCallbacks = [];
   #catchCallbacks = [];
@@ -54,22 +64,70 @@ class myPromise {
     this.#runCallbacks();
   }
 
+  /**
+   * Adds a callback function to be executed when the promise is fulfilled.
+   * @param {function} thenCallback - The callback function to be executed on fulfillment.
+   * @param {function} catchCallback - The callback function to be executed on rejection.
+   * @returns {myPromise} - A new myPromise instance.
+   */
   then(thenCallback, catchCallback) {
     return new myPromise((resolve, reject) => {
-      if (thenCallback != null) this.#thenCallbacks.push(thenCallback);
-      if (catchCallback != null) this.#catchCallbacks.push(catchCallback);
+      this.#thenCallbacks.push((result) => {
+        if (thenCallback == null) {
+          resolve(result);
+          return;
+        }
+
+        try {
+          resolve(thenCallback(result));
+        } catch (error) {
+          reject(error);
+        }
+      });
+
+      this.#catchCallbacks.push((result) => {
+        if (catchCallback == null) {
+          reject(result);
+          return;
+        }
+
+        try {
+          resolve(catchCallback(result));
+        } catch (error) {
+          reject(error);
+        }
+      });
 
       this.#runCallbacks();
     });
   }
 
+  /**
+   * Adds a callback function to be executed when the promise is rejected.
+   * @param {function} callback - The callback function to be executed on rejection.
+   */
   catch(callback) {
     this.then(undefined, callback);
     // this.#catchCallbacks.push(callback);
     // this.#runCallbacks();
   }
 
-  finally(callback) {}
+  /**
+   * Adds a callback function to be executed when the promise is settled (fulfilled or rejected).
+   * @param {function} callback - The callback function to be executed when the promise is settled.
+   */
+  finally(callback) {
+    return this.then(
+      (result) => {
+        callback();
+        return result;
+      },
+      (result) => {
+        callback();
+        throw result;
+      }
+    );
+  }
 }
 
 module.exports = myPromise;
